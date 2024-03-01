6b22d7ad0a9715c191b49fbeb17e351b82a3b5e8
Camila Lopes <camilalopesqta@gmail.com>
2024-02-29 21:09:13 -0300

    Exercícios de Styled-Components

--- src/Componentes/CardVideo.js
+++ src/Componentes/CardVideo.js
@@ -1,12 +1,14 @@
+import { BoxPaginaPrincipal, ImagemVideo, Titulo } from "./StyleCard";
+
 function CardVideo(props) {
   function reproduzVideo(props) {
     alert("O vídeo está sendo reproduzido");
   }
   return (
-    <div className="box-pagina-principal" onClick={reproduzVideo}>
-      <img src={props.image1} alt={props.textoAlternativo} />
-      <h4>{props.titulo}</h4>
-    </div>
+    <BoxPaginaPrincipal className="box-pagina-principal" onClick={reproduzVideo}>
+      <ImagemVideo src={props.image1} alt={props.textoAlternativo} />
+      <Titulo>{props.titulo}</Titulo>
+    </BoxPaginaPrincipal>
   );
 }
 
